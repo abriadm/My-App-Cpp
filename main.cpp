f8a988706d209863343ed7b5573b3a0acdb0eb89
Giveonaldo <giveonaldo@gmail.com>
2022-12-10 13:28:02 +0700

    Code V1.1

--- main.cpp
+++ main.cpp
@@ -7,8 +7,37 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+void play_game()
+{
+	int random = rand() % 11;
+	int guess;
+
+	cout << "\n###-> Welcome to Guessing Game <-###\n" << endl;
+
+	while (true)
+	{
+		cout << "Guess a number range 1-10: ";
+		cin >> guess;
+
+		if (guess == random)
+		{
+			cout << "You Win!!!" << endl;
+			break;
+		}
+		else if(guess < random)
+		{
+			cout << "Too low!!!!" << endl;
+		}
+		else
+		{
+			cout << "Too high!!!" << endl;
+		}
+	}
+}
+
 int main()
 {
+	srand(time(NULL));
 	char choice;
 	do
 	{
@@ -22,6 +51,7 @@ int main()
 		{
 		case 'A':
 			cout << "\nLet's play\n" << endl;
+			play_game();
 			break;
 		case 'B':
 			cout << "\nSee you next time!!!" << endl;
